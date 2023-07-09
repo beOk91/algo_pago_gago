8228bfb105faa18b1d1c3327ef869f719282b46d
Leejieun3001 <jieun3001@gmail.com>
2023-07-09 21:39:23 +0900

    10989

---
+++ src/lje/baekjoon/silver/beakjoon10989.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class beakjoon10989 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(bf.readLine());
+		int[] arr = new int[10001];
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		for (int i = 0; i<N;i++) {
+			
+			arr[Integer.parseInt(bf.readLine())]++;
+
+		}
+		bf.close();
+		
+		for(int i=0;i<10001;i++) {
+			while(arr[i]>0) {
+				sb.append(i).append('\n');
+				arr[i]--;
+			}
+		}
+		System.out.println(sb);
+		
+
+	}
+
+}
