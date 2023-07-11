40943a86a267dae59e1c78e1b46d3806b858d17a
Leejieun3001 <jieun3001@gmail.com>
2023-07-11 22:44:08 +0900

    commit test jieun

---
+++ src/lje/baekjoon/silver/test.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class test {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+	System.out.print("commit test");
+	}
+
+}
