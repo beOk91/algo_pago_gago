73855c0a1193f697a9f7b5d85e8f2715e4e0f931
minju <minju.kim27@cj.net>
2023-07-14 21:49:28 +0900

    BOJ 1920

---
+++ src/kmz/baekjoon/silver/sort/BOJ_1920.java
@@ -0,0 +1,36 @@
+package kmz.baekjoon.silver.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] list = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(list);
+
+        int m = Integer.parseInt(br.readLine());
+        int[] target = new int[m];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < m; i++) {
+            target[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (Arrays.binarySearch(list, target[i]) >= 0) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+        }
+    }
+}
