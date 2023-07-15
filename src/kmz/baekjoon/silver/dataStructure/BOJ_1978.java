0512dbc7826ad66ba144c73983969055fa5d4b04
minju <minju.kim27@cj.net>
2023-07-16 00:48:48 +0900

    BOJ 1978

---
+++ src/kmz/baekjoon/silver/dataStructure/BOJ_1978.java
@@ -0,0 +1,30 @@
+package kmz.baekjoon.silver.dataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1978 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int notDecimal = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (num == 1) {
+                notDecimal++;
+            }
+            for (int j = 2; j < num; j++) {
+                if (num % j == 0) {
+                    notDecimal++;
+                    break;
+                }
+            }
+        }
+        System.out.println(n - notDecimal);
+    }
+}
