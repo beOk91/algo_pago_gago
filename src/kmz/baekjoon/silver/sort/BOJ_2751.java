b9bc7e859de32e359a772fcae841c8113dbdc0e1
minju <minju.kim27@cj.net>
2023-07-16 22:52:49 +0900

    BOJ 2751

---
+++ src/kmz/baekjoon/silver/sort/BOJ_2751.java
@@ -0,0 +1,30 @@
+package kmz.baekjoon.silver.sort;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BOJ_2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(list);
+
+        for (Integer num : list) {
+            bw.write(String.valueOf(num));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
