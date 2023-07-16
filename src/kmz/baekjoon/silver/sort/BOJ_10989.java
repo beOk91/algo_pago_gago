cce247877df1a68e1db2f02aac9a8145cba3f0f0
minju <minju.kim27@cj.net>
2023-07-16 23:02:55 +0900

    BOJ 10989

---
+++ src/kmz/baekjoon/silver/sort/BOJ_10989.java
@@ -0,0 +1,26 @@
+package kmz.baekjoon.silver.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[10001];
+        for (int i = 0; i < n; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+        br.close();
+
+        for (int i = 0; i < 10001; i++) {
+            while (arr[i] > 0) {
+                sb.append(i).append('\n');
+                arr[i]--;
+            }
+        }
+        System.out.print(sb);
+    }
+}
