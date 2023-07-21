4760abc89cbc2806619e483746f6276decb64b56
beOk91 <be_ok91@naver.com>
2023-07-21 16:40:24 +0900

    [고범석] 백준 덱(10866), 정수삼각형(1932) 풀이

---
+++ src/lje/baekjoon/silver/Test.java
@@ -0,0 +1,10 @@
+package lje.baekjoon.silver;
+import java.io.IOException;
+
+public class Test {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+	System.out.print("commit test");
+	}
+
+}
