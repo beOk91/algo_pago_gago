6053be192e8c8fd0cb3d2b0356c25a9b74932bfb
beOk91 <be_ok91@naver.com>
2023-07-21 16:01:37 +0900

    [고범석] 백준 계단오르기(2579) 풀이

--- src/lje/baekjoon/silver/test.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class test {
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-	System.out.print("commit test");
-	}
-
-}
