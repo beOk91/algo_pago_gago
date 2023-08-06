9fece11f81960f6a22bb8edd646bb9d35ba55c5a
brocolliee <drop5890@honglab.org>
2023-08-06 17:03:25 +0900

    [juhyun] 0806_dots_sort

--- src/ljh/baekjoon/silver/baekjoon_1181.py
+++ src/ljh/baekjoon/silver/baekjoon_1181.py
@@ -1,5 +1,5 @@
-#  baekjoon 알고리즘 1026
-# 2023.08.05 이주현
+#  baekjoon 알고리즘 1181
+# 2023.08.06 이주현
 
 # 길이가 짧은 것
 # 길이가 같으면 -> 사전 순으로
@@ -15,7 +15,7 @@ for _ in range(num_of_words):
 
 sort_words_list = []
 for word in words_list:
-    sort_words_list.append((len(word), word))
+    sort_words_list.append((len(word), word)) # tuple 을 사용해서 sort 
 
 sort_words_list = sorted(sort_words_list)
 
