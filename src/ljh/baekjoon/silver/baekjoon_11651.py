bf6b704fcae4677804a533a7769ce2a56159efd9
brocolliee <drop5890@honglab.org>
2023-08-11 21:33:24 +0900

    [juhyun] 0811_dots_sort

---
+++ src/ljh/baekjoon/silver/baekjoon_11651.py
@@ -0,0 +1,19 @@
+#  baekjoon 알고리즘 11651
+# 2023.08.11 이주현
+
+import sys
+
+num_of_dots = int(sys.stdin.readline())
+
+dots_list = []
+for _ in range(num_of_dots):
+    dot = list(map(int, sys.stdin.readline().split()))
+    dot.reverse()
+    
+    dots_list.append(dot)
+
+print(dots_list)
+dots_list = sorted(dots_list) # list도 sort 가 먹는다!
+
+for dot in dots_list:
+    print(dot[1] , dot[0])
