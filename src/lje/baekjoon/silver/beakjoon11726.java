667bcfedb04b58166bea16226404795310519f35
Leejieun3001 <jieun3001@gmail.com>
2023-08-27 22:01:12 +0900

    [jieun] beakjoon11726

--- src/lje/baekjoon/silver/beakjoon11726.java
+++ src/lje/baekjoon/silver/beakjoon11726.java
@@ -1,5 +1,22 @@
 package lje.baekjoon.silver;
 
-public class beakjoon11726 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class beakjoon11726 {
+	public static void main(String args[]) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(bf.readLine());
+		int[] d = new int[n + 1];
+
+		d[0] = 1;
+		d[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			d[i] = d[i - 2] + d[i - 1];
+			d[i] %= 10007;
+		}
+
+		System.out.println(d[n]);
+	}
 }
