ed6205ef317fbc6ecb5553385d5baf66c3906df1
Leejieun3001 <jieun3001@gmail.com>
2023-09-10 01:15:34 +0900

    [jieun] beakjoon15711

---
+++ src/lje/baekjoon/silver/beakjoon15711.java
@@ -0,0 +1,67 @@
+package lje.baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class beakjoon15711 {
+
+		public static boolean[] isPrime = new boolean[2000001];
+	    public static List<Integer> list = new ArrayList<>();
+	    
+		public static void main(String[] args) throws NumberFormatException, IOException {
+			// TODO Auto-generated method stub
+			
+			BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+			int n = Integer.parseInt(bf.readLine());
+			getPrime();
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < n; i++) {
+				StringTokenizer st = new StringTokenizer(bf.readLine());
+				long a = Long.parseLong(st.nextToken());
+				long b = Long.parseLong(st.nextToken());
+				
+				
+				long sum = a + b;
+				if (sum < 4) {
+					sb.append("NO").append("\n");
+				} else if (sum % 2 == 0) {
+					sb.append("YES").append("\n");
+				} else {
+					if (check(sum - 2)) {
+						sb.append("NO").append("\n");
+					} else {
+						sb.append("YES").append("\n");
+					}
+				}
+
+			}
+			System.out.println(sb);
+		}
+
+		static boolean check(long num) {
+			if(num<=2000000) return isPrime[(int)num];
+			
+			for(int i=0;i<list.size();i++) {
+				if(num%list.get(i)==0) {
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		static void getPrime() {
+			isPrime[0]= isPrime[1]=true;
+			for(int i=2;i<=2000000;i++) {
+				if(!isPrime[i]) {
+					list.add(i);
+					for(int j=i*2;j<=2000000;j+=i) {
+						isPrime[j]=true;
+					}
+				}
+			}
+		}
+}
