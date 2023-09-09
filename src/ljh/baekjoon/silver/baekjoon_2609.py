abb0a4f9d6f038b9c5b8218b8d3bcc1862b2497a
brocolliee <drop5890@honglab.org>
2023-09-09 15:23:10 +0900

    [juhyun]0829_gcd_lcm_수정

--- src/ljh/baekjoon/silver/baekjoon_2609.py
+++ src/ljh/baekjoon/silver/baekjoon_2609.py
@@ -20,15 +20,3 @@ while True:
     min_num = mod
 
 
-
-# tmp = min_num
-# min_num = max_num % min_num
-# for i in range(min_num , 0 , -1): # 효율성
-#     if a % i == 0 and b % i == 0 :
-#         print(i)
-#         break
-
-# for i in range (max_num , (a*b) + 1):
-#     if i % a == 0 and i % b == 0 :
-#         print(i)
-#         break
