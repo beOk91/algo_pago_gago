56241e7e3952d5ba4f99fcf41aff947908853ec9
beOk91 <be_ok91@naver.com>
2023-09-10 23:43:01 +0900

    [고범석] 프로그래머스 정수 삼각형(43105)

---
+++ src/kbs/programmers/level3/Problem43105.java
@@ -0,0 +1,24 @@
+package kbs.programmers.level3;
+
+public class Problem43105 {
+	public int solution(int[][] triangle) {
+		int[][]dp=new int[triangle.length][triangle.length];
+		dp[0][0]=triangle[0][0];
+		int answer = 0;
+		for(int i=1;i<triangle.length;i++) {
+			for(int j=0;j<triangle[i].length;j++) {
+				if(j==0) {
+					dp[i][j]=dp[i-1][j]+triangle[i][j];
+				}else if(j==triangle[i].length-1) {
+					dp[i][j]=dp[i-1][j-1]+triangle[i][j];	
+				}else {
+					dp[i][j]=Math.max(dp[i-1][j-1],dp[i-1][j])+triangle[i][j];
+				}
+				if(i==triangle.length-1) {
+					answer=Math.max(dp[i][j],answer);
+				}
+			}
+		}
+        return answer;
+    }
+}
